a7f8e49c2c37202ea6cec74a192820e2abdc98aa
Juan Felipe Rojas Rodríguez <juan.rojas@iMac-de-libert4dm-3.local>
2026-03-11 14:19:15 -0600

    tarea1/

---
+++ tarea1/main.js
@@ -0,0 +1,10 @@
+let number = 10
+
+true
+false
+let resultado = (number >= 10 && number <=20)|| (number >= 40 && number <= 60)
+console.log ("el calculo de rangos para el numero " + number + "es :" + result)
+
+let Dogage = 10
+let humanage = 20 + (Dogage - 2) * 4;
+console.log("el perro de " + Dogage + "años tendria"+ humanage + "años si fuera humano")
